0f186573f1729273f3fd6166928b0f53ce5f0143
Jared Farkas <jaredmfarkas@gmail.com>
2023-02-10 15:39:38 -0800

    Add answers

--- Assets/Questions/QuestionSO.cs
+++ Assets/Questions/QuestionSO.cs
@@ -8,9 +8,20 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string question = "Enter New Question Text Here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
 
     public string GetQuestion(){
         return question;
     }
 
+    public string getAnswer(int i)
+    {
+        return answers[i];
+    }
+
+    public int getCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
 }
